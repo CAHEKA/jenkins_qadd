b8cf9480972e95fc48620f68fe75b288d535cb67
Kamil <k.sabuev@datamart.ru>
2023-01-02 16:13:25 +0300

    Initial commit

---
+++ src/main/java/ru/datamart/jenkins_qadd/JenkinsQaddApplication.java
@@ -0,0 +1,13 @@
+package ru.datamart.jenkins_qadd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsQaddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsQaddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/datamart/jenkins_qadd/JenkinsQaddApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.datamart.jenkins_qadd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsQaddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
